392a96c8a2bbbd3d2768dc791744db6165e50ccf
Alcibiades Bustillo <bustillo.alcibiades@gmail.com>
2020-08-30 12:38:56 -0400

    Model Post created

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,8 +1,12 @@
+"""Platzigram URLS module"""
+
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls'), name='posts'),
     path('posts/', include('posts.urls'), name='users'),
-]
+] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,31 @@
+#Django
 from django.contrib import admin
 
+from .models import Post
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Profile Admin"""
+    list_display = ('pk', 'user', 'title', 'photo')
+    list_display_links = ('pk', 'user',)
+    list_editable = ('title', 'photo')
+
+    search_fields = (
+        'title',                
+    )
+
+    list_filter = (        
+        'created',
+        'modified',        
+    )
+
+    fieldsets = (
+        ('Post', {
+            'fields': (('title', 'photo'),),
+        }),        
+        ('Metadata', {
+            'fields': (('created', 'modified'),),
+        }),
+    )
+    readonly_fields = ('created', 'modified')
