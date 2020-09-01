93d33c077ffa21f7ef99005c73c173a4fab74795
Alcibiades Bustillo <bustillo.alcibiades@gmail.com>
2020-09-01 19:06:36 -0400

    Login and Logout implementation

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('login/', views.loging_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+     path('signup/', views.signup, name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,16 @@
 """Users views"""
 # Django
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+
+# Exceptions
+from django.db.utils import IntegrityError
+
+# Models
+from django.contrib.auth.models import User
+from .models import Profile
+
 
 # Create your views here.
 def loging_view(request):
@@ -16,3 +25,34 @@ def loging_view(request):
         else:
             return render(request, 'users/login.html', {'error': 'Inavalid username and password'})
     return render(request, 'users/login.html')
+
+def signup(request):
+    """Sign up users"""
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
+        if password != password_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not mactch'})
+        
+        try:
+            user = User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is alredy taken.'})
+         
+        
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        profile = Profile(user=user)
+        profile.save()
+        return redirect('login')
+    return render(request, 'users/signup.html')
+
+# Create your views here.
+@login_required
+def logout_view(request):
+    """Logout user    
+    """
+    logout(request)
+    return redirect('login')
